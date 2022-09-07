cecdd8908eeb724a6c0b5455b88e9b014a01e696
Tivon <badboytjy@gmail.com>
2022-09-07 01:32:25 -0700

    Library project with class constructors

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # Library-Project-V2
+
+This is my Odin Project's Library Project.
+
+The Library App takes a users input about the title, author, page count, id number, and read status.
+
+It add each book to a table and it is saved with local storage!
+
+
--- script.js
+++ script.js
@@ -10,25 +10,7 @@ class Book {
 
 class UI {
   static displayBooks() {
-    const StoredBooks = [
-      {
-        title: "Book One",
-        author: "John Doe",
-        idNum: "1",
-        pages: 20,
-        status: true,
-      },
-
-      {
-        title: "Book Two",
-        author: "Jane Doe",
-        idNum: "2",
-        pages: 15,
-        status: false,
-      },
-    ];
-
-    const books = StoredBooks;
+    const books = Store.getBooks();
 
     books.forEach((book) => UI.addBookToList(book));
   }
@@ -36,17 +18,64 @@ class UI {
   static addBookToList(book) {
     const list = document.querySelector("#book-list");
 
-    const row = document.querySelector("tr");
+    const row = document.createElement("tr");
 
     row.innerHTML = `
         <td>${book.title}</td>
         <td>${book.author}</td>
         <td>${book.pages}</td>
         <td>${book.status}</td>
-        <td><a href="#" class="btn">X</a></td>
+        <td>${book.idNum}</td>
+        <td><a href="#" class="btn "><i class="delete fa-solid fa-trash-can fa-sm"></i></a></td>
         `;
 
     list.appendChild(row);
+  }
+
+  static deleteBook(el) {
+    if (el.classList.contains("delete")) {
+      el.parentElement.parentElement.parentElement.remove();
+    }
+  }
+
+  static clearFields() {
+    document.querySelector("#title").value = "";
+    document.querySelector("#author").value = "";
+    document.querySelector("#idnum").value = "";
+    document.querySelector("#pages").value = "";
+    document.querySelector("#status").checked = "";
+  }
+}
+// Local Storage
+class Store {
+  static getBooks() {
+    let books;
+    if (localStorage.getItem("books") === null) {
+      books = [];
+    } else {
+      books = JSON.parse(localStorage.getItem("books"));
+    }
+    return books;
+  }
+
+  static addBook(book) {
+    const books = Store.getBooks();
+
+    books.push(book);
+
+    localStorage.setItem("books", JSON.stringify(books));
+  }
+
+  static removeBook(idNum) {
+    const books = Store.getBooks();
+
+    books.forEach((book, index) => {
+      if (book.idNum === idNum) {
+        books.splice(index, 1);
+      }
+    });
+
+    localStorage.setItem("books", JSON.stringify(books));
   }
 }
 
@@ -56,16 +85,42 @@ document.querySelector("#book-form").addEventListener("submit", (e) => {
   e.preventDefault();
 
   // Get from values
-
   const title = document.querySelector("#title").value;
   const author = document.querySelector("#author").value;
   const idNum = document.querySelector("#idnum").value;
   const pages = document.querySelector("#pages").value;
   const status = document.querySelector("#status").checked;
 
-  // Instatiate book
+  // Validate
+  if (title === "" || author === "" || pages === "" || idNum === "") {
+    alert("Enter a valid input");
+  } else {
+    alert("Book Added");
+    // Instatiate book
+    const book = new Book(title, author, pages, idNum, status);
 
-  const book = new Book(title, author, pages, idNum, status);
+    // Add book to the UI
+    UI.addBookToList(book);
 
-  console.log(book);
+    // Add Book to store
+    Store.addBook(book);
+    console.log(book);
+
+    // Clear fields
+    UI.clearFields();
+  }
+});
+
+// Remove Book
+
+document.querySelector("#book-list").addEventListener("click", (e) => {
+  // remove book from UI
+  UI.deleteBook(e.target);
+
+  // Remove book from store
+  Store.removeBook(
+    e.target.parentElement.parentElement.previousElementSibling.textContent
+  );
+
+  alert("Book Removed");
 });
